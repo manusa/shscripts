d4a2022be9b6c7182aaa9ceb2282fca0dcee31da
Marc Nuri <marc@marcnuri.com>
2018-01-10 13:35:49 +0100

    Added error sample list

--- jmeter/jtlsummarize.sh
+++ jmeter/jtlsummarize.sh
@@ -19,15 +19,24 @@ ls $INPUT_FILE || { echo "File not found <$INPUT_FILE>"; exit 1 ; }
 WORK_FILE=$INPUT_FILE.temp
 OUTPUT_FILE=$INPUT_FILE.summary
 
+# Clear output file
+echo 'JMeter execution summary' > $OUTPUT_FILE
+printf "================================================================================\n">> $OUTPUT_FILE
+
 #Remove first line (Col titles) > sed -n '2,$ p' $INPUT_FILE |\
 TOTAL_COUNT=$(sed -n '2,$ p' $INPUT_FILE | cut -c -10,14- | sort | awk -F',' '{count++} END {print count}')
 ERROR_COUNT=$(sed -n '2,$ p' $INPUT_FILE | cut -c -10,14- | sort | awk -F',' '$8  == "false" {count++} END {print count}')
 
-echo "Total Samples: $TOTAL_COUNT\nEerrors: $ERROR_COUNT\n" >> $OUTPUT_FILE
+printf "Total Samples: $TOTAL_COUNT\nErrors: $ERROR_COUNT\n" >> $OUTPUT_FILE
 
 if [ $ERROR_COUNT -ne 0 ]; then
-echo "Threads with error:\n">> $OUTPUT_FILE
-echo "COUNT Thread">> $OUTPUT_FILE
-echo "===== ======">> $OUTPUT_FILE
-cat $INPUT_FILE | awk -F, 'NR>1 && $8 == "false"{arr[$6]++}END{for (a in arr) printf "%-5s %s\n", arr[a], a}' >> $OUTPUT_FILE
+    printf "================================================================================\n">> $OUTPUT_FILE
+    printf "Threads with error:\n\n">> $OUTPUT_FILE
+    printf "COUNT Thread\n">> $OUTPUT_FILE
+    printf "===== ======\n">> $OUTPUT_FILE
+    cat $INPUT_FILE | awk -F, 'NR>1 && $8 == "false"{arr[$6]++}END{for (a in arr) printf "%-5s %s\n", arr[a], a}' >> $OUTPUT_FILE
+    printf "\n\n">> $OUTPUT_FILE
+
+    printf "Failed samples:\n\n">> $OUTPUT_FILE
+    cat $INPUT_FILE | awk -F, 'NR>1 && $8 == "false"{printf "%s (%s)\n", $3, $6}' >> $OUTPUT_FILE
 fi
